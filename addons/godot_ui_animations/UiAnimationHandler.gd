d1b2ea2d5b9bcf596fd7440e5fcc7eaa0f7767db
Silver1078682 <ShinySmoothSilver@outmail.com>
2025-09-02 23:29:57 +0800

    improve UIAnimation class and make UIAnimator more customsizable

--- addons/godot_ui_animations/UiAnimationHandler.gd
+++ addons/godot_ui_animations/UiAnimationHandler.gd
@@ -5,140 +5,131 @@
 extends Node
 
 var default_offset := 8.0
-var default_speed := 0.3
+var default_duration := 0.3
 
 
 func get_node_center(node: CanvasItem) -> Vector2:
-	return (get_viewport().get_visible_rect().size.x / 2) - (node.size.x / 2)
+	return (get_viewport().get_visible_rect().size / 2) - (node.size / 2)
 
-
+## Recommended Ease : EaseOut
 func slide_from_side(
 	node: CanvasItem,
-	side := SIDE_TOP,
+	side: int,
+	tween: Tween,
 	offset := default_offset,
-	speed := default_speed,
+	duration := default_duration,
 ):
 #region slide from
 	match side:
 		SIDE_LEFT:
-			await slide_from_left(node, offset, speed)
+			await slide_from_left(node, tween, offset, duration)
 		SIDE_RIGHT:
-			await slide_from_right(node, offset, speed)
+			await slide_from_right(node, tween, offset, duration)
 		SIDE_BOTTOM:
-			await slide_from_bottom(node, offset, speed)
+			await slide_from_bottom(node, tween, offset, duration)
 		SIDE_TOP:
-			await slide_from_top(node, offset, speed)
+			await slide_from_top(node, tween, offset, duration)
 
 
-func slide_from_left(node, offset := default_offset, speed := default_speed):
+func slide_from_left(node, tween: Tween, offset := default_offset, duration := default_duration):
 	node.position.x = -node.size.x
-	await _custom_tween_property(node, "position:x", offset, speed, Tween.EASE_OUT).finished
+	await tween.tween_property(node, "position:x", offset, duration).finished
 
 
-func slide_from_right(node, offset := default_offset, speed := default_speed):
+func slide_from_right(node, tween: Tween, offset := default_offset, duration := default_duration):
 	node.position.x = get_viewport().size.x
 	var vp_size = get_viewport().get_visible_rect().size.x
 	var target = (vp_size - node.size.x) - offset
-	await _custom_tween_property(node, "position:x", target, speed, Tween.EASE_OUT).finished
+	await tween.tween_property(node, "position:x", target, duration).finished
 
 
-func slide_from_top(node, offset := default_offset, speed := default_speed):
+func slide_from_top(node, tween: Tween, offset := default_offset, duration := default_duration):
 	node.position.y = -node.size.y
-	await _custom_tween_property(node, "position:y", offset, speed, Tween.EASE_OUT).finished
+	await tween.tween_property(node, "position:y", offset, duration).finished
 
 
-func slide_from_bottom(node, offset := default_offset, speed := default_speed):
+func slide_from_bottom(node, tween: Tween, offset := default_offset, duration := default_duration):
 	node.position.y = get_viewport().size.y
 	var vp_size = get_viewport().get_visible_rect().size.y
 	var target = (vp_size - node.size.y) - offset
-	await _custom_tween_property(node, "position:y", target, speed, Tween.EASE_OUT).finished
+	await tween.tween_property(node, "position:y", target, duration).finished
 
 
 #endregion
 
 
+## Recommended Ease : EaseIn
 func slide_to_side(
 	node: CanvasItem,
-	side := SIDE_TOP,
+	side: int,
+	tween: Tween,
 	offset := default_offset,
-	speed := default_speed,
+	duration := default_duration,
 ):
 #region slide to
 	match side:
 		SIDE_LEFT:
-			await slide_to_left(node, offset, speed)
+			await slide_to_left(node, tween, offset, duration)
 		SIDE_RIGHT:
-			await slide_to_right(node, offset, speed)
+			await slide_to_right(node, tween, offset, duration)
 		SIDE_BOTTOM:
-			await slide_to_bottom(node, offset, speed)
+			await slide_to_bottom(node, tween, offset, duration)
 		SIDE_TOP:
-			await slide_to_top(node, offset, speed)
+			await slide_to_top(node, tween, offset, duration)
 
 
-func slide_to_left(node, offset := default_offset, speed := default_speed):
-	await _custom_tween_property(node, "position:x", -node.size.x, speed, Tween.EASE_IN).finished
+func slide_to_left(node, tween: Tween, offset := default_offset, duration := default_duration):
+	await tween.tween_property(node, "position:x", -node.size.x, duration).finished
 
 
-func slide_to_right(node, offset := default_offset, speed := default_speed):
-	await _custom_tween_property(node, "position:x", get_viewport().size.x, speed, Tween.EASE_IN).finished
+func slide_to_right(node, tween: Tween, offset := default_offset, duration := default_duration):
+	await tween.tween_property(node, "position:x", get_viewport().size.x, duration).finished
 
 
-func slide_to_top(node, offset := default_offset, speed := default_speed):
-	await _custom_tween_property(node, "position:x", -node.size.y, speed, Tween.EASE_IN).finished
+func slide_to_top(node, tween: Tween, offset := default_offset, duration := default_duration):
+	await tween.tween_property(node, "position:x", -node.size.y, duration).finished
 
 
-func slide_to_bottom(node, offset := default_offset, speed := default_speed):
-	await _custom_tween_property(node, "position:x", get_viewport().size.y, speed, Tween.EASE_IN).finished
+func slide_to_bottom(node, tween: Tween, offset := default_offset, duration := default_duration):
+	await tween.tween_property(node, "position:x", get_viewport().size.y, duration).finished
 
 
 #endregion
 
-
-func pop(node, speed := default_speed):
+## EaseOut
+func pop(node, tween: Tween, offset := default_offset, duration := default_duration):
 	node.show()
-	node.pivot_offset.x = node.size.x / 2
-	node.pivot_offset.y = node.size.y / 2
+	node.pivot_offset.x = node.size.x / 2 + offset
+	node.pivot_offset.y = node.size.y / 2 + offset
 	node.scale = Vector2.ZERO
 
-	await _custom_tween_property(node, "scale", Vector2.ONE, speed, Tween.EASE_OUT).finished
+	await tween.tween_property(node, "scale", Vector2.ONE, duration).finished
 
-
-func shrink(node, speed := default_speed):
-	await _custom_tween_property(node, "scale", Vector2.ZERO, speed, Tween.EASE_IN)
+## EaseIn
+func shrink(node, tween: Tween, offset := default_offset, duration := default_duration):
+	node.pivot_offset.x = node.size.x / 2 + offset
+	node.pivot_offset.y = node.size.y / 2 + offset
+	await tween.tween_property(node, "scale", Vector2.ZERO, duration)
 	node.hide()
 
-
-func fade_in(node: CanvasItem):
+## EaseIn
+func fade_in(node, tween: Tween, offset := default_offset, duration := default_duration):
 	node.show()
-	await _custom_tween_property(node, "modulate:a", 1, default_speed, Tween.EASE_IN).finished
+	await tween.tween_property(node, "modulate:a", 1, duration).finished
 
 
-func from_left_to_center(node):
-	await _custom_tween_property(node, "position:x", get_node_center(node), default_speed, Tween.EASE_OUT).finished
+func from_left_to_center(node, tween: Tween, offset := default_offset, duration := default_duration):
+	await tween.tween_property(node, "position:x", get_node_center(node).x, duration).finished
 
 
-func from_center_to_left(node):
-	await _custom_tween_property(node, "position:x", -node.size.x, default_speed, Tween.EASE_IN)
+func from_center_to_left(node, tween: Tween, offset := default_offset, duration := default_duration):
+	await tween.tween_property(node, "position:x", -node.size.x, duration)
 
 
-func from_right_to_center(node):
+func from_right_to_center(node, tween: Tween, offset := default_offset, duration := default_duration):
 	node.position.x = get_viewport().get_visible_rect().size.x
-	await _custom_tween_property(node, "position:x", get_node_center(node), default_speed, Tween.EASE_OUT)
+	await tween.tween_property(node, "position:x", get_node_center(node).x, duration)
 
 
-func from_center_to_right(node):
-	await _custom_tween_property(node, "position:x", node.size.x, default_speed, Tween.EASE_IN)
-
-
-func _create_custom_tween(ease: Tween.EaseType) -> Tween:
-	return create_tween().set_trans(Tween.TRANS_BACK).set_ease(ease)
-
-
-func _custom_tween_property(
-	object: Object,
-	property: NodePath,
-	final_val: Variant,
-	duration: float,
-	ease: Tween.EaseType,
-) -> PropertyTweener:
-	return _create_custom_tween(ease).tween_property(object, property, final_val, duration)
+func from_center_to_right(node, tween: Tween, offset := default_offset, duration := default_duration):
+	await tween.tween_property(node, "position:x", node.size.x, duration)
