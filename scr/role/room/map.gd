8180b427e888e29dae28f50d9a88557f57f7cdd5
Silver1078682 <ShinySmoothSilver@outmail.com>
2025-09-02 17:53:24 +0800

    add more comments for Map class

--- scr/role/room/map.gd
+++ scr/role/room/map.gd
@@ -1,4 +1,5 @@
 extends TileMapLayer
+## Map
 ## use built-in [TileMapLayer] to render textures and calculate terrian collision.
 
 static var _instance: TileMapLayer
@@ -21,16 +22,21 @@ static func reset() -> void:
 	_instance.clear()
 
 
+## convert a coordinate to position
+## the result will be exactly the center of the block
 static func to_pos(coord: Vector2i) -> Vector2:
 	return _instance.map_to_local(coord)
 
 
+## convert a position to coordinate
 static func to_coord(pos: Vector2) -> Vector2i:
 	return _instance.local_to_map(pos)
 
-## change a human-readble coordinate (y axis pointing up)  to a normal coordinate (y axis pointing down) 
+
+## change a human-readble coordinate (y axis pointing up)  to a normal coordinate (y axis pointing down)
 static func h2gui(coord: Vector2i) -> Vector2i:
 	return Vector2i(coord.x, Room.HEIGHT - coord.y - 1)
+
 
 static func update_block(coord: Vector2i) -> void:
 	BetterTerrain.update_terrain_cell(_instance, coord)
