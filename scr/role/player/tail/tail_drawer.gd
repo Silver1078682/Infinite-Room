6473d9549099eada26405bd8192e6ccd942cc615
Silver1078682 <ShinySmoothSilver@outmail.com>
2025-09-06 14:05:06 +0800

    BetterAnim@Tail:fix_strange_behviours_when_tail_get_stuck

--- scr/role/player/tail/tail_drawer.gd
+++ scr/role/player/tail/tail_drawer.gd
@@ -6,9 +6,19 @@ extends CharacterBody2D
 @export var min_length_between_nodes := 1.5
 @export var max_length_between_nodes := 2.0:
 	set(p_max_length_between_nodes):
-		max_length_between_nodes = clampf(p_max_length_between_nodes, min_length_between_nodes, INF)
+		max_length_between_nodes = maxf(p_max_length_between_nodes, min_length_between_nodes)
+
 
 func _process(_delta: float) -> void:
+	# If the tip of the tail(which collide with terrain) is too far away from the root.
+	# It's likely that it is STUCK! we therefore need to remove its collision mask on terrain.
+
+	_update_cnt += 1
+	if _update_cnt >= PHYSICS_ENABLE_CHECK_LOOP:
+		var body: RigidBody2D = get_child(-1)
+		var enabled = body.position.length_squared() <= MAX_PHYSICS_ENABLE_LENGTH_SQUARED
+		body.collision_mask = 1 if enabled else 0
+
 	line2d.clear_points()
 	var root_position = Vector2.ZERO
 	for i in get_children():
@@ -17,6 +27,11 @@ func _process(_delta: float) -> void:
 			p = limit_from(root_position, p)
 			line2d.add_point(p)
 			root_position = p
+
+
+const PHYSICS_ENABLE_CHECK_LOOP := 5
+var _update_cnt := PHYSICS_ENABLE_CHECK_LOOP
+const MAX_PHYSICS_ENABLE_LENGTH_SQUARED := 300.0
 
 
 func limit_from(from: Vector2, to: Vector2) -> Vector2:
