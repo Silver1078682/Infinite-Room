fd317fe5cae92665f16b2c9da52b4fbd9740c2a2
Silver1078682 <ShinySmoothSilver@outmail.com>
2025-09-09 15:28:50 +0800

    Rename: base to base_room

---
+++ scr/role/room/theme/base_room.gd
@@ -0,0 +1,7 @@
+extends RoomTheme
+# base room is not a "base class" of room,
+# it's just a literal base room
+
+func _spawn(_room: Room):
+	Log.info("Spawning base room...")
+	#SL.load_room("base")
